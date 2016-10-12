1a074b9909f193684546145c677eb5c177ff6549
vagrant <vagrant@localhost.localdomain>
2016-10-12 13:47:40 -0400

    updating vim.sh to handle init

---
+++ vim.sh
@@ -0,0 +1,11 @@
+## Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+## Pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+cp -r .vim/* ~/.vim/
+cp .vimrc ~/
+
+
